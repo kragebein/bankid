de9ec147ec53e99a77825035665357c9c7ab1acf
Stian Langvann <krage@firi-082fc9c.firi.com>
2022-02-25 16:47:06 +0100

    Updated with backend logging

--- bankid/classes.py
+++ bankid/classes.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass
 from bankid.users import Users
 
+
 @dataclass(frozen=True)
 class Status:
-    statuscode:int = 0
-    meaning:str = 'BIDI initialiseres og vil hente data innen 30 sekunder eller mindre'
-    text:str = 'Initialiserer'
-    extra:str = ''
-    color:str = 'black'
-    
+    statuscode: int = 0
+    meaning: str = 'BIDI initialiseres og vil hente data innen 30 sekunder eller mindre'
+    text: str = 'Initialiserer'
+    extra: str = ''
+    color: str = 'black'
+
     def __repr__(self):
         return self.statuscode
 
@@ -18,32 +19,31 @@ class Status:
 
 @dataclass
 class Auth:
-    is_authorized: bool = None
+    is_auth: bool = False
     user: dict = None
 
     def __init__(self, key):
         self.x = Users()
-        self.key = key
-        if self.key in self.x.userlist:
-            self.user = self.x.userlist[self.key]
-            self.is_authorized = True
+        if self.x.check(key):
+            print(self.x.user)
+            self.is_auth = True
+            self.user = self.x.user
 
     def get(self):
         ''' Gets the user data'''
-        if not self.is_authorized:
+        if not self.is_auth:
             return []
-        return self.x.userlist[self.key]
+        return self.user
 
     def __str__(self):
-        return self.is_authorized
+        return self.is_auth
 
     def __repr__(self):
-        return self.is_authorized
-    
+        return self.is_auth
+
 
 @dataclass
 class Api:
     message: str = None
     bidi: str = None
     openapi: str = None
-
